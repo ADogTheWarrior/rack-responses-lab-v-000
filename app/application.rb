31c04e05bef86440ccee82b2f39e6bc808bcd497
Adam Fudge <coolfudge@gmail.com>
2017-11-16 21:02:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,10 @@ class Application
     resp = Rack::Response.new
 
     time= Time.new
-puts "#{Time.now.hour}"
-    if  time.hour >= 0 && time.hour < 12
+# puts "#{Time.now.hour}"
+    if  time.now.hour >= 0 && time.now.hour < 12
       resp.write "Good Morning!"
-    elsif time.hour >= 12 && time.hour < 24
+    elsif time.now.hour >= 12 && time.now.hour < 24
       resp.write "Good Afternoon!"
     end
 
